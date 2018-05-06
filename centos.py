ed784469f101d81c2673fe46366c280dcc8db44c
4815162342lost <family-gan@yandex.ru>
2018-05-07 00:01:31 +0300

    Update centos.py

--- centos.py
+++ centos.py
@@ -31,144 +31,76 @@ need_patching = not_need_patching = error_count = 0
 
 packages_which_require_reboot = ("glibc", "hal", "systemd", "udev")
 
-def write_to_excel_file(content_updates_pkgs, content_all_pkgs, sheet_name, conten_type, idx):
+def write_to_excel_file(content_updates_pkgs, content_all_pkgs, idx, sheet):
     """Function to write content to xlsx-file"""
     global need_patching
     global not_need_patching
     global error_count
     kernel_update = reboot_require = "no"
-    format_kernel = format_reboot = format['format_green']
+    format_kernel = format_reboot = format_potential_risky_packages = format['format_green']
     no_potential_risky_packages = "yes"
-    format_potential_risky_packages = format['format_green']
-    column0_width = column1_width = column2_width = 0
+    column_width=[]
+    column_width.append(max(len(key) for key in content_updates_pkgs.keys()))
+    column_width.append(max(len(str(value)) for value in content_updates_pkgs.values()))
+    column_width.append(max(len(key) for key in content_updates_pkgs.keys()))
+    counter = 0
+    # avoid the bug #41479 https://github.com/saltstack/salt/issues/41479
     try:
-        sheet = xls_file.add_worksheet(sheet_name)
-    except Exception as e:
-        error_count += 1
-        total_sheet.write(idx + 2, 1, "Can not create sheet", format['format_purple'])
-        total_sheet.write(idx + 2, 2, "", format['format_bold'])
-        total_sheet.write(idx + 2, 0, str(sheet_name), format['format_purple'])
-        total_sheet.write(idx + 2, 3, "Unknown", format['format_purple'])
-        total_sheet.write(idx + 2, 4, "Unknown", format['format_purple'])
-        total_sheet.write(idx + 2, 5, "Unknown", format['format_purple'])
-        termcolor.cprint(
-            'Error occured during creation the sheet %s. Perhaps you have two or more same servers in server_list.txt file' % sheet_name,
-            color='red', on_color='on_white')
-        return None
-    if conten_type == "patches":
-        counter = 0
-        # avoid the bug #41479 https://github.com/saltstack/salt/issues/41479
-        try:
-            content_updates_pkgs.pop("retcode")
-            content_all_pkgs.pop("retcode")
-        except KeyError:
-            pass
-        for key, value in sorted(content_updates_pkgs.items()):
-            if len(key) > column0_width:
-                column0_width = len(key)
-            if len(str(value)) > column1_width:
-                column1_width = len(value)
-            # if new packages (dependence) will be installed with upgrade
-            try:
-                if len(str(content_all_pkgs[key])) > column2_width:
-                    column2_width = len(str(content_all_pkgs[key]))
-            except KeyError:
-                pass
-            if no_potential_risky_packages == "yes":
-                for current_bad_package in settings['bad_packages']:
-                    if str(key).startswith(current_bad_package):
-                        no_potential_risky_packages = "no"
-                        format_potential_risky_packages = format['format_red']
-                        break
-            if kernel_update == "no":
-                if str(key).startswith("kernel") == True or str(key).startswith("linux-image") == True:
-                    kernel_update = "yes"
-                    format_kernel = format['format_red']
-                    reboot_require = "yes"
-                    format_reboot = format['format_red']
-            sheet.write(counter + 2, 0, key, format['format_border'])
-            try:
-                sheet.write(counter + 2, 1, content_all_pkgs[key], format['format_border'])
-            except KeyError:
-                sheet.write(counter + 2, 1, "new packages (will be installed as dependency)", format['format_border'])
-            sheet.write(counter + 2, 2, value, format['format_border'])
-            counter += 1
+        content_updates_pkgs.pop("retcode")
+        content_all_pkgs.pop("retcode")
+    except KeyError:
+        pass
+    for key, value in sorted(content_updates_pkgs.items()):
+        if no_potential_risky_packages == "yes":
+            for current_bad_package in settings['bad_packages']:
+                if str(key).startswith(current_bad_package):
+                    no_potential_risky_packages = "no"
+                    format_potential_risky_packages = format['format_red']
+                    break
         if kernel_update == "no":
-            for current_package in packages_which_require_reboot:
-                if current_package in content_updates_pkgs.keys():
+            if str(key).startswith("kernel") == True or str(key).startswith("linux-image") == True:
+                kernel_update = "yes"
+                format_kernel = format['format_red']
+                reboot_require = "yes"
+                format_reboot = format['format_red']
+        sheet.write(counter + 2, 0, key, format['format_border'])
+        try:
+            sheet.write(counter + 2, 1, content_all_pkgs[key], format['format_border'])
+        except KeyError:
+            sheet.write(counter + 2, 1, "new packages (will be installed as dependency)", format['format_border'])
+        sheet.write(counter + 2, 2, value, format['format_border'])
+        counter += 1
+    if kernel_update == "no":
+        for current_package in packages_which_require_reboot:
+            if current_package in content_updates_pkgs.keys():
+                reboot_require = "yes"
+                format_reboot = format['format_red']
+                break
+        if reboot_require == "no":
+            for current_package in content_updates_pkgs.keys():
+                if current_package.find("-firmware-") != -1:
                     reboot_require = "yes"
                     format_reboot = format['format_red']
                     break
-            if reboot_require == "no":
-                for current_package in content_updates_pkgs.keys():
-                    if current_package.find("-firmware-") != -1:
-                        reboot_require = "yes"
-                        format_reboot = format['format_red']
-                        break
-        sheet.set_column(0, 0, width=column0_width + 2)
-        sheet.set_column(1, 1, width=column1_width + 2)
-        sheet.set_column(2, 2, width=column2_width + 2)
-        # if patching is not required
-        if counter == 0:
-            not_need_patching += 1
-            sheet.set_column(0, 0, width=50)
-            sheet.set_tab_color("#79eca3")
-            sheet.write(0, 0, "All packages are up to date. Upgrade is not required", format['format_bold'])
-            total_sheet.write(idx + 2, 1, "All packages are up to date. Upgrade is not required", format['format_green'])
-            total_sheet.write(idx + 2, 2, "", format['format_bold'])
-            total_sheet.write(idx + 2, 3, kernel_update, format_kernel)
-            total_sheet.write(idx + 2, 4, reboot_require, format_reboot)
-            total_sheet.write(idx + 2, 5, no_potential_risky_packages, format_potential_risky_packages)
-            total_sheet.write(idx + 2, 0, str(sheet_name), format['format_green'])
-        # if only one patch required
-        elif counter == 1:
-            servers_for_patching.append(sheet.get_name())
-            need_patching += 1
-            sheet.set_tab_color("#FF7373")
-            sheet.write(0, 0, "Only 1 package need to upgrade", format['format_bold'])
-            sheet.write(1, 0, "Package name", format['format_bold'])
-            sheet.write(1, 1, "Current version", format['format_bold'])
-            sheet.write(1, 2, "Available version", format['format_bold'])
-            total_sheet.write(idx + 2, 1, "Only 1 package need to upgrade", format['format_red'])
-            total_sheet.write(idx + 2, 2, "", format['format_bold'])
-            total_sheet.write(idx + 2, 3, kernel_update, format_kernel)
-            total_sheet.write(idx + 2, 4, reboot_require, format_reboot)
-            total_sheet.write(idx + 2, 5, no_potential_risky_packages, format_potential_risky_packages)
-            total_sheet.write(idx + 2, 0, str(sheet_name), format['format_red'])
-        # more one patch required
-        else:
-            servers_for_patching.append(sheet.get_name())
-            need_patching += 1
-            sheet.set_tab_color("#FF7373")
-            sheet.write(0, 0, str(counter) + " packages need to upgrade", format['format_bold'])
-            sheet.write(1, 0, "Package name", format['format_bold'])
-            sheet.write(1, 1, "Current version", format['format_bold'])
-            sheet.write(1, 2, "Available version", format['format_bold'])
-            total_sheet.write(idx + 2, 2, "", format['format_bold'])
-            total_sheet.write(idx + 2, 3, kernel_update, format_kernel)
-            total_sheet.write(idx + 2, 4, reboot_require, format_reboot)
-            total_sheet.write(idx + 2, 5, no_potential_risky_packages, format_potential_risky_packages)
-            total_sheet.write(idx + 2, 1, str(counter) + " packages need to upgrade", format['format_red'])
-            total_sheet.write(idx + 2, 0, str(sheet_name), format['format_red'])
-    if conten_type == "error":
-        error_count += 1
-        sheet.set_tab_color("#cb87fb")
-        sheet.set_column(0, 0, 45)
-        sheet.write(0, 0, "Fatal error", format['format_bold'])
-        total_sheet.write(idx + 2, 1, "Fatal error", format['format_purple'])
-        total_sheet.write(idx + 2, 2, "", format['format_bold'])
-        total_sheet.write(idx + 2, 0, str(sheet_name), format['format_purple'])
-        total_sheet.write(idx + 2, 3, "Unknown", format['format_purple'])
-        total_sheet.write(idx + 2, 4, "Unknown", format['format_purple'])
-        total_sheet.write(idx + 2, 5, "Unknown", format['format_purple'])
-
+    for c in range(3):
+        sheet.set_column(c,c,width=column_width[c]+2)
+    total_sheet.write(idx + 2, 3, kernel_update, format_kernel)
+    total_sheet.write(idx + 2, 4, reboot_require, format_reboot)
+    total_sheet.write(idx + 2, 5, no_potential_risky_packages, format_potential_risky_packages)
+    if counter > 0:
+        need_patching += 1;
+        servers_for_patching.append(sheet.get_name())
+    else:
+        not_need_patching += 1
+    write_to_total_sheet(counter, "", sheet, total_sheet, format, idx, 'centos')
 
 def main_function():
+    global error_count
     file= open('./server_list.txt', 'r')
     server_list = open('./server_list.txt', 'r').read().rstrip().split('\n')
     file.close()
 
-    print(','.join(server_list))
+    print("Starting to collect patching list for servers: " + ','.join(server_list))
 
     try:
         proc_get_updates = subprocess.Popen("salt -L '" + ','.join(
@@ -193,8 +125,6 @@ def main_function():
                                 stdout_get_updates)
     stdout_get_updates == re.sub("minion .* was already deleted from tracker, probably a duplicate key", "",
                                  stdout_get_updates)
-
-
     proc_out_get_updates_json = json.loads(stdout_get_updates)
     stdout_get_all_pkgs = re.sub("Minion .* did not respond. No job will be sent.", "", stdout_get_all_pkgs)
     stdout_get_all_pkgs = re.sub("No minions matched the target. No command was sent, no jid was assigned.", "",
@@ -207,13 +137,20 @@ def main_function():
     error_list_from_xlsx = []
     for idx, current_server in enumerate(server_list):
         try:
-            write_to_excel_file(proc_out_get_updates_json[current_server], proc_out_get_all_pkgs_json[current_server],
-                                current_server, "patches", idx)
+            sheet = xls_file.add_worksheet(current_server)
+            write_to_excel_file(proc_out_get_updates_json[current_server], proc_out_get_all_pkgs_json[current_server], idx, sheet)
         except KeyError:
             error_list_from_xlsx.append(current_server)
-            write_to_excel_file(None, None, current_server, "error", idx)
+            write_to_total_sheet("unknown error", "error", sheet, total_sheet, format, idx, 'centos')
+            sheet.set_column(0, 0, width=17)
+            error_count+=1
+        except Exception as e:
+            error_list_from_xlsx.append(current_server)
+            termcolor.cprint(
+                'Error occured during creation the sheet %s. Perhaps you have two or more same servers in server_list.txt file' % current_server,
+                color='red', on_color='on_white')
     if error_list_from_xlsx:
-        termcolor.cprint("There are problem with following servers:\n" + ',\n'.join(error_list_from_xlsx), color='red',
+        termcolor.cprint("There are problem with following servers:\n" + ', '.join(error_list_from_xlsx), color='red',
                          on_color='on_white')
     add_chart(need_patching, not_need_patching, error_count, xls_file, total_sheet, format)
     xls_file.close()
@@ -237,7 +174,7 @@ termcolor.cprint(
     ", // ,,/ ,.// ,/ ,// / /, // ,/, /, // ,/,\n/, // ,/,_|_// ,/ ,, ,/, // ,/ /, //, /,/\n /, /,.-'   '-. ,// ////, // ,/,/, // ///\n, ,/,/         \ // ,,///, // ,/,/, // ,\n,/ , ^^^^^|^^^^^ ,// ///  /,,/,/, ///, //\n / //     |  O    , // ,/, //, ///, // ,/\n,/ ,,     J\/|\_ |+'(` , |) ^ ||\|||\|/` |\n /,/         |   || ,)// |\/-\|| ||| |\] .\n/ /,,       /|    . ,  ///, . /, // ,//, /\n, / /,/     \ \    ). //, ,( ,/,/, // ,/,",
     color='blue', on_color='on_grey')
 print("Starting to collect of all patches...")
-xlsx_name = 'Unix_List_of_updates_' + str(today.strftime("%B_%Y")) + "_Centos.xlsx"
+xlsx_name = 'Linix_list_of_updates_' + str(today.strftime("%B_%Y")) + "_Centos.xlsx"
 xls_file = xlsxwriter.Workbook(xlsx_name)
 format=create_formats(xls_file)
 total_sheet=create_total_sheet(xls_file, format)
