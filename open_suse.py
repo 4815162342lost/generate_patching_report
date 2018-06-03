1343b5d809f57c8397244611cb61dfb214b5405c
4815162342lost <family-gan@yandex.ru>
2018-06-03 23:51:13 +0300

    Update open_suse.py

--- open_suse.py
+++ open_suse.py
@@ -11,11 +11,8 @@ import socket
 import sys
 sys.path.append('./modules/')
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
-from auto_mm import *
 from create_excel_template import *
-from send_email import *
 from main import *
-from auto_snapshots import *
 
 
 settings=get_settings()
@@ -109,24 +106,9 @@ def main_function():
             security_patches_list.append(splitted_line[6].rstrip())
         write_to_file(sheet, idx_glob, security_patches_list, need_reboot)
         security_patches_list.clear()
-    if args.csv == 'yes' and servers_for_patching:
-        error_list_from_csv = working_with_csv(servers_for_patching, db_cur, today, 'open_suse')
-        if error_list_from_csv:
-            termcolor.cprint("Maintenance mode will be incorrect:\n" + ',\n'.join(error_list_from_csv), color='magenta',
-                              on_color='on_white')
-    if args.snap=='yes' and servers_for_patching:
-        servers_whcih_require_snap_without_additional_activities=snap_determine_needed_servers(db_cur, servers_for_patching)
-        snap_create_csv_file(db_cur, servers_whcih_require_snap_without_additional_activities, "auto-snapshots_open_suse_{month}.csv".format(month=today.strftime("%B")), today)
-    if args.csv == 'yes' or args.snap=='yes':
-        db_cur.close()
     add_chart(need_patching, not_need_patching, error_count, xls_file, total_sheet, format)
     xls_file.close()
-    if args.email != None:
-        send_mail(args.email, settings['email_from'], settings['smtp_server'],  xlsx_name, today, 'Patching list for Open_Suse ')
-        print("All done, the file \"{file_name}\" has been sent to e-mail {mail_address}".format(file_name=xlsx_name,
-                                                                                                 mail_address=args.email))
-    else:
-        print("All done. Please, see the file \"" + xlsx_name + "\". Have a nice day!")
+    perform_additional_actions(args, today, 'open_suse', xlsx_name, settings, servers_for_patching)
 
 
 termcolor.cprint("____________________________________________________________________\n                                                 _,-\"-._        tbk\n                 <_     _>\n     _____----\"----________________________________`---'_______\n    /----------------------------------------------------------\ \n   /] [_] #### [___] #### [___]  \/  [___] #### [___] #### [_] [\ \n  /----------------------------11407-----------------------|-|---\ \n  |=          S  B  B                          C  F  F     |_|  =|\n[=|______________________________________________________________|=]\n   )/_-(-o-)=_=(=8=)=_=(-o-)-_ [____] _-(-o-)=_=(=8=)=_=(-o-)-_\(\n====================================================================\nSBB CFF FFS  Ae 6/6  (1952)  Co'Co'  125 km/h  4300 kW", color='red', on_color='on_white')
@@ -135,6 +117,5 @@ xls_file = xlsxwriter.Workbook(xlsx_name)
 format=create_formats(xls_file)
 total_sheet=create_total_sheet(xls_file, format)
 create_xlsx_legend(total_sheet, format)
-db_cur=sqlite(args.csv, args.snap)
 
 main_function()
