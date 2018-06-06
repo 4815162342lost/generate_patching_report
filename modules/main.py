6d9b52bf1b8cd13c5c98e0be35ccc258fa8b0346
4815162342lost <family-gan@yandex.ru>
2018-06-06 23:01:21 +0300

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -24,18 +24,17 @@ def perform_additional_actions(args, today, os, xlsx_name, settings, servers_for
         # open database or not
         db_con = sqlite3.connect('./patching.db')
         db_cur = db_con.cursor()
-    else:
-        return 0
-    if args.csv=='yes':
-        error_list_from_csv = working_with_csv(servers_for_patching, db_cur, today, os)
-        if error_list_from_csv:
-            termcolor.cprint("Maintenance mode will be incorrect:\n" + ',\n'.join(error_list_from_csv), color='magenta', on_color='on_white')
-    if args.snap=='yes':
-        servers_whcih_require_snap_without_additional_activities=snap_determine_needed_servers(db_cur, servers_for_patching)
-        snap_create_csv_file(db_cur, servers_whcih_require_snap_without_additional_activities, "auto-snapshots_{os}_{month}.csv".format(month=today.strftime("%B"), os=os), today)
-    db_con.close()
+        if args.csv=='yes':
+            error_list_from_csv = working_with_csv(servers_for_patching, db_cur, today, os)
+            if error_list_from_csv:
+                termcolor.cprint("Maintenance mode will be incorrect:\n" + ',\n'.join(error_list_from_csv), color='magenta', on_color='on_white')
+        if args.snap=='yes':
+            servers_whcih_require_snap_without_additional_activities=snap_determine_needed_servers(db_cur, servers_for_patching)
+            snap_create_csv_file(db_cur, servers_whcih_require_snap_without_additional_activities, "auto-snapshots_{os}_{month}.csv".format(month=today.strftime("%B"), os=os), today)
+        db_con.close()
     if args.email != None:
         send_mail(args.email, settings['email_from'], settings['smtp_server'],  xlsx_name, today, 'Patching list for {os} '.format(os=os))
         print("All done, the file \"{file_name}\" has been sent to e-mail {mail_address}".format(file_name=xlsx_name, mail_address=args.email))
     else:
         print("All done. Please, see the file \"" + xlsx_name + "\". Have a nice day!")
+
