f54958897060daf82b5c7fc8ead09fd599d0339a
4815162342lost <family-gan@yandex.ru>
2018-07-29 20:19:22 +0300

    Update create_excel_template.py

--- modules/create_excel_template.py
+++ modules/create_excel_template.py
@@ -1,7 +1,7 @@
 def create_formats(xls_file):
     # create xslsx-file's template
     format_colors=('format_red', 'format_green', 'format_purple', 'format_yellow', 'format_gray',
-                   'format_blue', 'format_bold', 'format_border', 'format_kernel', 'format_reboot')
+                   'format_blue', 'format_bold', 'format_border', 'format_kernel', 'format_reboot', 'format_green_url', 'format_red_url', 'format_purple_url')
 
     format={}
     for current_format in format_colors:
@@ -15,6 +15,12 @@ def create_formats(xls_file):
     format['format_gray'].set_bg_color("#a3a3a3")
     format['format_blue'].set_bg_color("#87cad8")
     format['format_bold'].set_bold()
+    format['format_green_url'].set_bg_color("#96d67c")
+    format['format_red_url'].set_bg_color("#ffa7a7")
+    format['format_purple_url'].set_bg_color("#d195ec")
+    for current_url_format in ('format_green_url', 'format_red_url', 'format_purple_url'):
+        format[current_url_format].set_font_color("blue")
+        format[current_url_format].set_underline()
     return format
 
 
@@ -95,29 +101,24 @@ def write_to_total_sheet(content, patching_type, sheet, total_sheet, format, idx
             sheet.set_column(0, 0, width=21)
             sheet.write(0, 0, "Update is not required", format['format_bold'])
             total_sheet.write(idx_glob + 2, 1, "All packages are up to date", format['format_green'])
-            total_sheet.write(idx_glob + 2, 0, sheet.get_name(), format['format_green'])
-        elif content == 1:
-            sheet.set_tab_color("#FF7373")
-            sheet.write(0, 0, "Only one {security}patch is available".format(security=patching_type), format['format_bold'])
-            sheet.write(1, 0, 'Package name', format['format_bold'])
-            if os!='open_suse':
-                sheet.write(1, 1, 'Current version', format['format_bold'])
-                sheet.write(1, 2, 'Available version', format['format_bold'])
-            total_sheet.write(idx_glob + 2, 1, "Only 1 {security}patch is available".format(security=patching_type), format['format_red'])
-            total_sheet.write(idx_glob + 2, 0, sheet.get_name(), format['format_red'])
+            total_sheet.write_url(row=idx_glob + 2, col=0, url="internal:'{sheet_name}'!A1".format(sheet_name=sheet.get_name()), string=sheet.get_name(), cell_format=format['format_green_url'])
         else:
             sheet.set_tab_color("#FF7373")
             sheet.write(1, 0, 'Package name', format['format_bold'])
             if os!='open_suse':
                 sheet.write(1, 1, 'Current version', format['format_bold'])
                 sheet.write(1, 2, 'Available version', format['format_bold'])
-            sheet.write(0, 0, str(content) + " {security}patches are available".format(security=patching_type), format['format_bold'])
+            if content==1:
+                sheet.write(0, 0, "Only one {security}patch is available".format(security=patching_type),
+                            format['format_bold'])
+            else:
+                sheet.write(0, 0, str(content) + " {security}patches are available".format(security=patching_type), format['format_bold'])
             total_sheet.write(idx_glob + 2, 1, str(content) + " {security}patсhes are available".format(security=patching_type),
                               format['format_red'])
-            total_sheet.write(idx_glob + 2, 0, sheet.get_name(), format['format_red'])
+            total_sheet.write_url(row=idx_glob + 2, col=0, url="internal:'{sheet_name}'!A1".format(sheet_name=sheet.get_name()), string=sheet.get_name(), cell_format=format['format_red_url'])
     else:
         total_sheet.write(idx_glob + 2, 1, "error: " + str(content), format['format_purple'])
-        total_sheet.write(idx_glob + 2, 0, sheet.get_name(), format['format_purple'])
+        total_sheet.write_url(row=idx_glob + 2, col=0, url="internal:'{sheet_name}'!A1".format(sheet_name=sheet.get_name()), string=sheet.get_name(), cell_format=format['format_purple_url'])
         total_sheet.write(idx_glob + 2, 3, "unknown", format['format_purple'])
         total_sheet.write(idx_glob + 2, 4, "unknown", format['format_purple'])
         sheet.set_tab_color("purple")
