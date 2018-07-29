e65905d8e92d6798d9bcffc7814a1533d1b9aa1e
4815162342lost <family-gan@yandex.ru>
2018-07-29 18:05:09 +0300

    Update debian.py

--- debian.py
+++ debian.py
@@ -6,7 +6,6 @@ import os
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 import datetime
 import re
-import socket
 
 import sys
 sys.path.append('./modules/')
@@ -28,12 +27,8 @@ def write_to_file(contenr, type, sheet, idx_glob):
     global need_patching
     global not_need_patching
     if type == 'patch':
-        kernel_update = "no"
-        format_kernel = format['format_green']
-        reboot_require = "no"
-        format_reboot = format['format_green']
-        no_potential_risky_packages = "yes"
-        format_potential_risky_packages = format['format_green']
+        kernel_update = reboot_require = "no"
+        format_kernel = format_reboot = format['format_green']
         column0_width = column1_width = column2_width = 10
         if reboot_require=='no' and 'systemd' in contenr.keys():
             reboot_require='yes'
@@ -45,12 +40,6 @@ def write_to_file(contenr, type, sheet, idx_glob):
                 column2_width=len(contenr[current_patch][0])
             if len(contenr[current_patch][1])>column1_width:
                 column1_width=len(contenr[current_patch][1])
-            if no_potential_risky_packages=='yes':
-                for current_bad in settings['bad_packages']:
-                    if current_patch.startswith(current_bad):
-                        no_potential_risky_packages='no'
-                        format_potential_risky_packages=format['format_red']
-                        break
             if re.search('linux-image.+', current_patch):
                 kernel_update = reboot_require = 'yes'
                 format_kernel = format_reboot = format['format_red']
@@ -59,7 +48,6 @@ def write_to_file(contenr, type, sheet, idx_glob):
             sheet.write(col + 2, 2, contenr[current_patch][0])
         total_sheet.write(idx_glob + 2, 3, kernel_update, format_kernel)
         total_sheet.write(idx_glob + 2, 4, reboot_require, format_reboot)
-        total_sheet.write(idx_glob + 2, 5, no_potential_risky_packages, format_potential_risky_packages)
         sheet.set_column(0, 0, width=column0_width)
         sheet.set_column(1, 1, width=column1_width)
         sheet.set_column(2, 2, width=column2_width)
@@ -73,14 +61,16 @@ def write_to_file(contenr, type, sheet, idx_glob):
         global error_count
         error_count+=1
         servers_with_error.append(sheet.get_name())
+        sheet.write(0,0,"Unknow error", format["format_bold"])
+        sheet.set_tab_color("purple")
         total_sheet.write(idx_glob + 2, 1, "error: " + str(contenr), format['format_purple'])
         total_sheet.write(idx_glob + 2, 0, sheet.get_name(), format['format_purple'])
         total_sheet.write(idx_glob + 2, 3, "unknown", format['format_purple'])
         total_sheet.write(idx_glob + 2, 4, "unknown", format['format_purple'])
-        total_sheet.write(idx_glob + 2, 5, "unknown", format['format_purple'])
 
 
 def main_function():
+    global error_count
     servers_list=open('./server_list.txt')
     ssh_con=paramiko.SSHClient()
     ssh_con.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -94,23 +84,28 @@ def main_function():
     for idx_glob, current_server in enumerate(servers_list.readlines()):
         current_server=current_server.rstrip()
         print('Working with {server} server...'.format(server=current_server))
-        ssh_con.connect(hostname=current_server, username='root', pkey=ssh_key, timeout=30, port=22)
-        stdin_check, stdout_check, stderr_check = ssh_con.exec_command(command='apt update',  timeout=1200)
-        a=stderr_check.read().decode()
-        stdin_check, stdout_check, stderr_check = ssh_con.exec_command(command="unattended-upgrade --dry-run -d 2>/dev/null | grep 'Checking' | awk '{ print $2 }'", timeout=1200)
-        print('Trying to perform apt list --upgradable command')
-        stdin_all_version, stdout_all_version, stderr_all_version = ssh_con.exec_command(command="apt list --upgradable 2>/dev/null | tail -n +2", timeout=1200)
-        stdout_check_1=stdout_check.read().decode().rstrip('\n').split('\n')
-        stdout_all_version_1=stdout_all_version.read().decode().rstrip('\n').split('\n')
-        ssh_con.close()
-        sheet = xls_file.add_worksheet(current_server)
-        for current_package in stdout_all_version_1:
-            current_package_formated = current_package.split(' ')
-            if current_package_formated[0][:str(current_package_formated).find('/') - 2] in stdout_check_1:
-                all_packages[current_package_formated[0][:str(current_package_formated).find('/') - 2]] = (
-                current_package_formated[1], current_package_formated[5][:-1])
-        write_to_file(all_packages, 'patch', sheet, idx_glob)
-        all_packages.clear()
+        try:
+            sheet = xls_file.add_worksheet(current_server)
+            ssh_con.connect(hostname=current_server, username='root', pkey=ssh_key, timeout=30, port=22)
+            stdin_check, stdout_check, stderr_check = ssh_con.exec_command(command='apt update',  timeout=1200)
+            a=stderr_check.read().decode()
+            stdin_check, stdout_check, stderr_check = ssh_con.exec_command(command="unattended-upgrade --dry-run -d 2>/dev/null | grep 'Checking' | awk '{ print $2 }'", timeout=1200)
+            print('Trying to perform apt list --upgradable command')
+            stdin_all_version, stdout_all_version, stderr_all_version = ssh_con.exec_command(command="apt list --upgradable 2>/dev/null | tail -n +2", timeout=1200)
+            stdout_check_1=stdout_check.read().decode().rstrip('\n').split('\n')
+            stdout_all_version_1=stdout_all_version.read().decode().rstrip('\n').split('\n')
+            ssh_con.close()
+            for current_package in stdout_all_version_1:
+                current_package_formated = current_package.split(' ')
+                if current_package_formated[0][:str(current_package_formated).find('/') - 2] in stdout_check_1:
+                    all_packages[current_package_formated[0][:str(current_package_formated).find('/') - 2]] = (
+                    current_package_formated[1], current_package_formated[5][:-1])
+            write_to_file(all_packages, 'patch', sheet, idx_glob)
+            all_packages.clear()
+        except:
+            write_to_file("Connection issue", "error", sheet, idx_glob)
+            print('Critical error, skip this server...')
+    add_chart(need_patching, not_need_patching, error_count, xls_file, total_sheet, format)
     xls_file.close()
     perform_additional_actions(args, today, 'debian', xlsx_name, settings, servers_for_patching)
 
