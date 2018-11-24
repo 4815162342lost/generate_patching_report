2401f03a0422d81e0d28627139de46065263699d
4815162342lost <family-gan@yandex.ru>
2018-11-25 00:59:33 +0300

    Update send_separate_emails.py

--- automatization_scripts/send_separate_emails.py
+++ automatization_scripts/send_separate_emails.py
@@ -16,7 +16,10 @@ import csv
 import datetime
 import configparser
 import logging
+import random
 
+
+log_file=logging.basicConfig(filename=os.path.dirname(os.path.realpath(__file__))+ '/send_separate_emails_error.log', level=logging.INFO,  datefmt="%d/%m%Y %H:%M:%S", format="%(asctime)s %(message)s")
 
 def get_settings():
     '''parse the config file'''
@@ -40,6 +43,7 @@ def return_server_groups(server_list):
                 server_groups[so_email[0]].append(current_server)
         except TypeError:
             termcolor.cprint("Error: {server} server is not found on local database, ignoring...".format(server=current_server), color="white", on_color="on_red" )
+            logging.warning('Error: {server} server is not found on local database, ignoring...'.format(server=current_server))
     #example: {'Faith.Connor@my_retard.com,Zoey.Coatcher@my_retard.com': ['cent_os', 'secret_server'], 'Lous.Coatcher@myretard.com,Our_team@my_retard.com': ['server22']}
     return server_groups
 
@@ -60,7 +64,7 @@ def prepare_xlsx_file(servers):
         #crete sheet with server name and open txt-file with server
         server_sheet=xlsx_file.add_worksheet(current_server.upper())
         server_file_csv=csv.reader(open(current_server.lower(), 'r'), delimiter=';')
-        #extract the line which related with current server from total.txt file, save this line to patches_str variable
+        #extract the line which related with current server from total.csv file, save this line to patches_str variable
         for row in csv_reader:
             if row[0]==current_server:
                 patches_str=row;
@@ -68,13 +72,23 @@ def prepare_xlsx_file(servers):
         #write the patches to xlsx-sheet and set width
         if 'patches_str' not in locals():
             termcolor.cprint('Can not find the {server} server in total.csv file. Skipping...'.format(server=current_server), color="white", on_color="on_red")
+            logging.warning('Can not find the {server} server in total.csv file. Skipping...'.format(server=current_server))
             servers_which_not_find_in_total_csv_file+=1
             continue
         if int(patches_str[3])!=0:
+            suse=False
             server_sheet.write(0,0, "{count} packages will be updated".format(count=patches_str[3]), format['format_bold'])
             server_sheet.write_row(1, 0, next(server_file_csv)[0:3], cell_format=format['format_bold'])
             for idx1, current_patch in enumerate(server_file_csv):
-                server_sheet.write_row(idx1+2, 0, current_patch, cell_format=format['format_border'])
+                if idx1==0 and current_patch[1]=='none':
+                    suse=True
+                    server_sheet.write(1, 0, 'Update description', format['format_bold'])
+                    for i in range(1,3):
+                        server_sheet.write(1, i, ' ')
+                if not suse:
+                    server_sheet.write_row(idx1+2, 0, current_patch, cell_format=format['format_border'])
+                else:
+                    server_sheet.write(idx1 + 2, 0, current_patch[0], format['format_border'])
             for current_width in range(4, 7):
                 server_sheet.set_column(current_width - 4, current_width - 4, width=int(patches_str[current_width]))
             server_sheet.set_tab_color(color="red")
@@ -88,7 +102,7 @@ def prepare_xlsx_file(servers):
             total_sheet.write_url(row=idx + 1, col=0,url="internal:'{sheet_name}'!A1".format(sheet_name=current_server.upper()), cell_format=format['format_green_url'], string=current_server.upper())
         total_sheet.conditional_format(first_row=idx + 1, first_col=2, last_row=idx + 1, last_col=3, options={'type': 'text', 'criteria': 'containing', 'value': 'yes', 'format': format['format_red']})
         total_sheet.conditional_format(first_row=idx + 1, first_col=2, last_row=idx + 1, last_col=3, options={'type': 'text', 'criteria': 'containing', 'value': 'no', 'format': format['format_green']})
-        col_width=(15,34,14,14)
+        col_width=(20,34,14,14)
         for i in range(0,4):
             total_sheet.set_column(i,i,col_width[i])
     xlsx_file.close()
@@ -99,32 +113,25 @@ def prepare_xlsx_file(servers):
         return 1
 
 
-
-def send_email_with_xlsx_to_customer(group_of_servers):
-    '''group_of_servers -- service owners (only names)'''
-    return 0
-    names=group_of_servers.split(',')
+def send_email_with_xlsx_to_customer(group_of_e_mails):
+    '''group_of_e_mails -- e-mails of service owners (only emails)'''
+    names = db_cur.execute("SELECT SERVICE_OWNERS FROM SERVER_OWNERS_EMAILS WHERE CONTACT_EMAILS=:e_mails LIMIT 1",{'e_mails': group_of_e_mails}).fetchone()[0].split(",")
     final_names=[n.split(' ')[0] for n in names]
     if len(final_names)>1:
         final_names=', '.join(final_names[:-1]) + " and " + final_names[-1]
     else:
         final_names=final_names[0]
-    e_mails=db_cur.execute("SELECT CONTACT_EMAILS FROM SERVER_OWNERS_EMAILS WHERE SERVICE_OWNERS=:group_of_servers", {'group_of_servers' : group_of_servers}).fetchone()
     mail_body="<html><head></head><body>\
-    <p><font color=f02a00>This is a test message, not real, please, ignore. I am only need a real e-mails for perform several tests with new script</font></p>\
+    <p><font color=f02a00>This is a test messages, not real, please, ignore. I am only need a real e-mails for perform several tests with new script</font></p>\
     <p>Hello {names},</p>\
     \
-    <p>This is e-mail regarding to list of updates for future Linux patching cycle. \
+    <p>This is an e-mail with list of updates for future monthly Linux patching cycle. \
     In attached Excel-file you can find servers with available patches.</p>\
     \
     <p><b><u>Please, note:</b></u><br>\
-    1) Patches list have been gather recently. During month new patches may be released and we will install all of them<br>\
-    2) We can exclude several patches which can affect to application stability<br>\
-    3) All servers can be rebooted if new kernel or packages which require reboot will be released during month<br>\
-    4) Patches which can affect a application stability can be exluded from patching cycle. This is mean that server can still vulnerable to several threat<br>\
-    5) Server will not be patched if no patches in attached xlsx-file.</p>\
-    <p>Please, reply to this e-mail if you have any concerns\questions. Also reply to this e-mail if you want to change patching contacts for your servers./<p>\
-    Have a nice day!\
+    Тут будет какой-то очень важный и нужный текст, который ещё не придуман\
+    <br>\
+    <p>Please, reply to this e-mail if you have any concerns or questions.</p>\
     {sign}</body></html>".format(names=final_names, sign=settings["sign"])
     msg = MIMEMultipart('related')
     msg_a = MIMEMultipart('alternative')
@@ -144,20 +151,25 @@ def send_email_with_xlsx_to_customer(group_of_servers):
     logo.close()
     part4.add_header('Content-ID', '<logo>')
     msg.attach(part4)
-    msg['Subject'] = "[TEST MESSAGE, PLEASE IGNORE] Upcomming Linux patching -- {month}, list of updates".format(month=today.strftime("%B"))
+    msg['Subject'] = "[ONE MORE TEST ROUND, PLEASE IGNORE] Upcomming Linux patching -- {month}, list of updates".format(month=today.strftime("%B"))
     msg['From'] = settings['email_from']
-    msg['To'] = ','.join(e_mails)
+    msg['To'] = group_of_e_mails
     msg['Cc'] = settings['e_mail_cc']
-    return 0
     try:
         s = smtplib.SMTP(settings['smtp_server'])
-        s.sendmail(msg['From'], msg['To'].split(',') + settings['e_mail_cc'].split(','), msg.as_string())
+        s.sendmail(msg['From'], group_of_e_mails.split(",") + settings['e_mail_cc'].split(','), msg.as_string())
         s.quit()
-        print('e_mail was sent correctly')
-    except Exception as e:
-        termcolor.cprint('Error occured during sendig e-mail. Exception: ', color='red', on_color='on_white')
-        print(e)
-        return None
+        termcolor.cprint('E_mail was sent correctly to {e_mails}!'.format(e_mails=msg['To']), color="white", on_color="on_green")
+    except:
+        termcolor.cprint("Can not send the e-mail to {e_mails} first time, trying again...".format(e_mails=msg['To']), color="red", on_color="on_white")
+        try:
+            s = smtplib.SMTP(settings['smtp_server'])
+            s.sendmail(msg['From'], group_of_e_mails.split(",") + settings['e_mail_cc'].split(','), msg.as_string())
+            s.quit()
+            termcolor.cprint('E_mail was sent correctly in this time!', color="white", on_color="on_green")
+        except Exception as e:
+            termcolor.cprint('Error occured during sendig e-mail again... Skipping this message.  Exception: {ex} '.format(ex=str(e)), color='red', on_color='on_white')
+            logging.warning("Can not send the e-mail to {e_mails}".format(e_mails=msg['To']))
 
 def main():
     '''main function'''
@@ -167,18 +179,19 @@ def main():
     #get the unique so with affected servers as dict, see example in function description
     uniq_so_e_mails_group_with_servers=return_server_groups(servers_list)
     #generate xlsx-file for each new group
-    for current_group_of_project, current_uniq_so_e_mails_group_servers in uniq_so_e_mails_group_with_servers.items():
+    for e_mails, servers in uniq_so_e_mails_group_with_servers.items():
         #print if need send e-mail with xlsx-file
-        if prepare_xlsx_file(current_uniq_so_e_mails_group_servers)==0:
-            send_email_with_xlsx_to_customer(current_group_of_project)
+        if prepare_xlsx_file(servers)==0:
+            send_email_with_xlsx_to_customer(e_mails)
         else:
-            termcolor.cprint("E-mail for these {current_uniq_so_group_servers} server(s) will not be send to customer due to errors above...".format(current_uniq_so_group_servers=current_uniq_so_e_mails_group_servers), color="white", on_color="on_red")
-
+            termcolor.cprint("E-mail for these {current_uniq_so_group_servers} server(s) will not be send to customer due to errors above...".format(current_uniq_so_group_servers=servers), color="white", on_color="on_red")
+            logging.warning("Error: e-mail for these {current_uniq_so_group_servers} server(s) will not be send to customer...".format(current_uniq_so_group_servers=servers))
 
 db_cur=sqlite3.connect('./patching_dev.db').cursor()
 settings = get_settings()
 today=datetime.datetime.now()
 
+termcolor.cprint('\n                    ,\n                   dM\n                   MMr\n                  4MMML                  .\n                  MMMMM.                xf\n  .              "M6MMM               .MM-\n   Mh..          +MM5MMM            .MMMM\n   .MMM.         .MMMMML.          MMMMMh\n    )MMMh.        MM5MMM         MMMMMMM\n     3MMMMx.     \'MMM3MMf      xnMMMMMM"\n     \'*MMMMM      MMMMMM.     nMMMMMMP\"\n       *MMMMMx    \"MMM5M\    .MMMMMMM=\n        *MMMMMh   \"MMMMM\"   JMMMMMMP\n          MMMMMM   GMMMM.  dMMMMMM            .\n           MMMMMM  \"MMMM  .MMMMM(        .nnMP\"\ \n..          *MMMMx  MMM\"  dMMMM\"    .nnMMMMM*\n \"MMn...     \'MMMMr \'MM   MMM\"   .nMMMMMMM*"\n  "4MMMMnn..   *MMM  MM  MMP\"  .dMMMMMMM\"\"\n    ^MMMMMMMMx.  *ML \"M .M*  .MMMMMM**\"\n       *PMMMMMMhn. *x > M  .MMMM**\"\"\n          \"\"**MMMMhx/.h/ .=*\"\n                   .3P\"%....\n        [nosig]  nP\"     \"*MMnx', on_color='on_green', color="white")
 try:
     os.chdir(os.path.dirname(os.path.realpath(__file__)) + '/' + today.strftime("%b_%Y") + '_separate_csv_with_patching_list/')
 except FileNotFoundError:
