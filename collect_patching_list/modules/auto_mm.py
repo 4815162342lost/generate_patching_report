c30fe436559d4c331b8ec194454ae8a37662f9f6
4815162342lost <family-gan@yandex.ru>
2019-01-05 16:46:28 +0300

    Update auto_mm.py

--- collect_patching_list/modules/auto_mm.py
+++ collect_patching_list/modules/auto_mm.py
@@ -6,24 +6,25 @@ import pytz
 
 logging.getLogger(__name__)
 
-def working_with_csv(servers_for_patching, db_cur, today, os, need_csv_mm, time_zone):
+def working_with_csv(servers_for_patching, db_cur, today, os, time_zone):
     '''Function for raise other function with csv-creation from auto_mm.py file'''
-    servers_for_write_to_csv_mm, servers_with_additional_monitors, servers_for_write_to_csv_snapshots,\
+    servers_for_write_to_csv_mm, servers_for_write_to_csv_without_mm, servers_with_additional_monitors,\
     servers_for_write_to_csv_notify_before_4_days, error_list_from_csv = create_csv_list_with_servers_for_write_and_with_additional_monitors(servers_for_patching, db_cur, today, time_zone)
-    if need_csv_mm=='yes':
+    if servers_for_write_to_csv_mm:
         write_to_csv('linux_MM_{date}_patching_{name}'.format(date=today.strftime("%b_%Y"), name=os), servers_for_write_to_csv_mm, 'long')
         print('Hey, csv-file linux_MM_{date}_patching_{name}.csv has been compiled!'.format(date=today.strftime("%b_%Y"), name=os))
         if servers_with_additional_monitors:
             write_to_csv('linux_MM_CIS_{date}_patching_{name}'.format(date=today.strftime("%b_%Y"), name=os),
                          set(servers_with_additional_monitors), "long")
             print("FYI: csv-file linux_MM_CIS_{date}_patching_{name}.csv created!".format(date=today.strftime("%b_%Y"), name=os))
-        if servers_for_write_to_csv_notify_before_4_days:
-            write_to_csv('linux_e_mail_notify_before_4_days_{date}_patching_{name}'.format(date=today.strftime("%b_%Y"), name=os), set(servers_for_write_to_csv_notify_before_4_days), 'short')
-            print("FYI: csv-file linux_e_mail_notify_before_4_days_{date}_patching_{name}.csv created!".format(date=today.strftime("%b_%Y"), name=os))
-        if servers_for_write_to_csv_snapshots:
-            write_to_csv('linux_snapshots_{date}_patching_{name}'.format(date=today.strftime("%b_%Y"), name=os), set(servers_for_write_to_csv_snapshots), 'short')
-            print("FYI: csv-file linux_snapshots_{date}_patching_{name}.csv created!".format(date=today.strftime("%b_%Y"), name=os))
+    if servers_for_write_to_csv_notify_before_4_days:
+        write_to_csv('linux_e_mail_notify_before_4_days_{date}_patching_{name}'.format(date=today.strftime("%b_%Y"), name=os), set(servers_for_write_to_csv_notify_before_4_days), 'short')
+        print("FYI: csv-file linux_e_mail_notify_before_4_days_{date}_patching_{name}.csv created!".format(date=today.strftime("%b_%Y"), name=os))
+    if servers_for_write_to_csv_without_mm:
+        write_to_csv('linux_without_MM_{date}_patching_{name}'.format(date=today.strftime("%b_%Y"), name=os), servers_for_write_to_csv_without_mm, 'long')
+        print('Hey, csv-file linux_without_MM_{date}_patching_{name}.csv has been compiled!'.format(date=today.strftime("%b_%Y"), name=os))
     return error_list_from_csv
+
 
 def get_patching_start_date(today, window_code, db_cur):
     '''function for return patching start date (year, minth and day)'''
@@ -45,6 +46,7 @@ def get_patching_start_date(today, window_code, db_cur):
         patch_day = calendar_for_month[patch_code_from_db[0]][patch_code_from_db[1]]
     return datetime.datetime(year=patch_year, month=patch_month, day=patch_day)
 
+
 def write_to_csv(csv_name, list_for_write, csv_format):
     '''function for generate csv-files'''
     responsible_user='Ilyas Ganiev'
@@ -60,19 +62,20 @@ def write_to_csv(csv_name, list_for_write, csv_format):
             for current_list_for_write in list_for_write:
                 csv_mm_writer.writerow([current_list_for_write[0], current_list_for_write[1]])
 
+
 def create_csv_list_with_servers_for_write_and_with_additional_monitors(servers_for_patching, db_cur, today, time_zone_from_settings):
     '''return list with mm plan'''
-    servers_for_write_to_csv=[]; servers_with_additional_monitors=[]; error_list=[]; snapshots_list=[]; servers_before_4_days=[]
+    servers_for_write_to_csv_with_mm=[]; servers_with_additional_monitors=[];
+    error_list=[]; servers_before_4_days=[]; servers_without_mm = []
     for current_server in servers_for_patching:
-        server_window_code, need_mm, need_snapshot, need_email_before_four_days= db_cur.execute('SELECT WINDOW_CODE, NEED_MM, NEED_SNAPSHOT, NEED_EMAIL_BEFORE_4_DAYS FROM SERVERS \
+        server_window_code, need_mm, need_email_before_four_days= db_cur.execute('SELECT WINDOW_CODE, NEED_MM, NEED_EMAIL_BEFORE_4_DAYS FROM SERVERS \
                                             WHERE SERVER_NAME=:current_server COLLATE NOCASE',
                                             {'current_server':current_server}).fetchone()
         if not server_window_code:
             error_list.append('Server {server} does not exist on database...'.format(server=current_server))
             continue
         if need_mm==0:
-            print("For server {server} maintenance mode is not required... Skip it")
-            continue
+            print("For server {server} maintenance mode is not required")
         #get patching start day
         patching_start_date=get_patching_start_date(today, server_window_code, db_cur)
         server_name_from_db, time_zone, patching_start_time, patching_duration, additional_monitors=db_cur.execute('SELECT SERVER_NAME, TIMEZONE, START_TIME, DURATION_TIME, ADDITIONAL_MONITORS FROM SERVERS\
@@ -86,18 +89,17 @@ def create_csv_list_with_servers_for_write_and_with_additional_monitors(servers_
         patching_start_datetime=patching_start_datetime.astimezone(pytz.timezone(time_zone_from_settings))
         #get patching end time
         patching_end_datetime=patching_start_datetime+datetime.timedelta(hours=int(patching_duration[0:2]), minutes=int(patching_duration[3:]))
-        servers_for_write_to_csv.append((server_name_from_db, patching_start_datetime.strftime('%d.%m.%Y %H:%M'), patching_end_datetime.strftime('%d.%m.%Y %H:%M'), ''))
-        if need_snapshot==1:
-            snapshots_list.append((server_name_from_db, patching_start_datetime.strftime('%d.%m.%Y %H:%M')))
+        if need_mm == 1:
+            servers_for_write_to_csv_with_mm.append((server_name_from_db, patching_start_datetime.strftime('%d.%m.%Y %H:%M'), patching_end_datetime.strftime('%d.%m.%Y %H:%M'), ''))
+            if additional_monitors == 1:
+                additional_cis = db_cur.execute('SELECT ADDITIONAL_CIS, ADITIONAL_MONITOR_NAME FROM ADDITIONAL_MONITORS WHERE SERVER_NAME=:current_server COLLATE NOCASE', {'current_server': current_server}).fetchall()
+                if not additional_cis:
+                    error_list.append("Error: For server {server_name} should be additional monitors...".format(server_name=current_server))
+                else:
+                    for current_cis in additional_cis:
+                        servers_with_additional_monitors.append((current_cis[0], patching_start_datetime.strftime('%d.%m.%Y %H:%M'), patching_end_datetime.strftime('%d.%m.%Y %H:%M'), current_cis[1]))
+        elif need_mm == 0:
+            servers_without_mm.append((server_name_from_db, patching_start_datetime.strftime('%d.%m.%Y %H:%M'), patching_end_datetime.strftime('%d.%m.%Y %H:%M'), ''))
         if need_email_before_four_days==1:
             servers_before_4_days.append((server_name_from_db, patching_start_datetime.strftime('%d.%m.%Y %H:%M')))
-        if additional_monitors == 1:
-            additional_cis =db_cur.execute('SELECT ADDITIONAL_CIS, ADITIONAL_MONITOR_NAME FROM ADDITIONAL_MONITORS\
-                           WHERE SERVER_NAME=:current_server COLLATE NOCASE',
-                           {'current_server' : current_server}).fetchall()
-            if not additional_cis:
-                error_list.append("Error: For server {server_name} should be additional monitors...".format(server_name=current_server))
-                continue
-            for current_cis in additional_cis:
-                servers_with_additional_monitors.append((current_cis[0], patching_start_datetime.strftime('%d.%m.%Y %H:%M'), patching_end_datetime.strftime('%d.%m.%Y %H:%M'), current_cis[1]))
-    return servers_for_write_to_csv, servers_with_additional_monitors, snapshots_list, servers_before_4_days, error_list
+    return servers_for_write_to_csv_with_mm, servers_without_mm, servers_with_additional_monitors, servers_before_4_days, error_list
